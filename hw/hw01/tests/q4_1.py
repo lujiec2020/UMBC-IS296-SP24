ba30b52f22e2d5c135962711007c7d1741f991b5
lujiec2020 <lujiec2020@gmail.com>
2024-02-04 13:23:34 -0500

    lab01 and hw1

---
+++ hw/hw01/tests/q4_1.py
@@ -0,0 +1,2 @@
+OK_FORMAT = True
+test = {'name': 'q4_1', 'points': 1, 'suites': [{'cases': [{'code': '>>> 1 <= jobs_q1 <= 3\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ hw/hw01/tests/q5_2.py
@@ -0,0 +1,4 @@
+OK_FORMAT = True
+test = {   'name': 'q5_2',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> isinstance(smallest_change_major, (int, float))\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
